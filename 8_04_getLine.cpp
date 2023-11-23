5bfef237e1274ebad6b9a5a2d1bf362fc138740b
zamfiroiu <alin.zamfiroiu@csie.ase.ro>
2023-11-23 12:32:36 +0200

    Add files via upload

---
+++ 8_04_getLine.cpp
@@ -0,0 +1,45 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//void main() {
+//
+//
+//	//cout << "Introduceti text:";
+//	//string cuvant;
+//	//cin >> cuvant;
+//	//cout << "Ati introdus: " << cuvant << endl;
+//
+//	//int variabila;
+//	//string valoareIntreaga;
+//	//getline(cin,valoareIntreaga);
+//	//variabila=atoi(valoareIntreaga.c_str());
+//	//cout << variabila;
+//
+//	//cout << "Introduceti text:";
+//	//string propozitie;
+//	//getline(cin,propozitie);
+//	//cout << "Ati introdus: " << propozitie << endl;
+//
+//	cout << "Introduceti text:";
+//	char* altaPropozitie2=new char[30];
+//	cin.get(altaPropozitie2,'/n');
+//	cin.get();
+//	cout << "Ati introdus: " << altaPropozitie2 << endl;
+//	
+//	//int valoare; 
+//	//char* valoareString = new char[20];
+//	//cout << "Valoare:";
+//
+//	//cin.getline(valoareString, 20);
+//	//valoare = atoi(valoareString);
+//	//cout << valoare;
+//
+//	//cout << "Introduceti text2:";
+//	//char* altaPropozitie = new char[20];
+//	////cin.getline(altaPropozitie, 20);
+//	//cin.getline(altaPropozitie, 20);
+//	//cout << "Ati introdus: " << altaPropozitie << endl;
+//
+//}
